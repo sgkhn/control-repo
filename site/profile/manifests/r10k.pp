c6b7b8f9429fc63272229cb24104624cc97fa94a
sgkhn <149669555+sgkhn@users.noreply.github.com>
2023-11-17 13:07:35 +0300

    remove r10k webhook conf

--- site/profile/manifests/r10k.pp
+++ site/profile/manifests/r10k.pp
@@ -2,12 +2,4 @@ class profile::r10k {
   class {'r10k':
     remote => 'https://github.com/sgkhn/control-repo',
   }
-  class {'r10k::webhook::config':
-    use_mcollective => false,
-    enable_ssl      => false,
-  }
-  class {'r10k::webhook':
-    user  => 'root',
-    group => 'root',
-  }
 }
